11075a3942c1813e6c56167b42ccafb199495cc0
beepboop271 <53926222+beepboop271@users.noreply.github.com>
2021-04-07 00:32:16 -0400

    move command listening to its own file
    functionality that uses event listeners should be independent of another, eg the logging system might ignore messages that the command system doesn't ignore, and would listen to events that the command system does not listen to

--- src/command.ts
+++ src/command.ts
@@ -1,6 +1,43 @@
 import { Client, Message } from "discord.js";
+import fs from "fs";
+
+import { config } from "./config";
+
+type CommandExecutor = (client: Client, msg: Message, args: string[]) => void;
 
 export interface Command {
   name: string;
-  execute(client: Client, msg: Message, args: string[]): void;
+  execute: CommandExecutor;
 }
+
+const commands: Map<string, CommandExecutor> = new Map();
+
+for (const file of fs.readdirSync("dist/commands")) {
+  if (file.endsWith(".js")) {
+    // tslint:disable-next-line: no-unsafe-any
+    const command: Command = (await import(`./commands/${file}`)).command;
+    commands.set(command.name, command.execute);
+  }
+}
+
+export const registerCommands = (bot: Client): void => {
+  bot.on("message", (msg): void => {
+    if (msg.author.bot) {
+      return;
+    }
+
+    if (!msg.content.startsWith(config.prefix)) {
+      return;
+    }
+
+    const args = msg.content.substring(config.prefix.length).split(/\s+/g);
+    if (args.length < 1) {
+      return;
+    }
+    const command = commands.get(args[0]);
+    if (command === undefined) {
+      return;
+    }
+    command(bot, msg, args);
+  });
+};
--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,13 @@
-import { Client, Message } from "discord.js";
-import fs from "fs";
+import { Client } from "discord.js";
 
-import { Command } from "./command";
-import { config } from "./config";
+import { registerCommands } from "./command";
 
 const bot = new Client();
-const commands: Map<string, (msg: Message, args: string[]) => void> = new Map();
-
-for (const file of fs.readdirSync("dist/commands")) {
-  if (file.endsWith(".js")) {
-    // tslint:disable-next-line: no-unsafe-any
-    const command: Command = (await import(`./commands/${file}`)).command;
-    commands.set(command.name, command.execute.bind(undefined, bot));
-  }
-}
-
-bot.on("message", (msg): void => {
-  if (msg.author.bot) {
-    return;
-  }
-  if (msg.guild === null) {
-    return;
-  }
-
-  if (!msg.content.startsWith(config.prefix)) {
-    return;
-  }
-
-  const args = msg.content.substring(config.prefix.length).split(/\s+/g);
-  if (args.length < 1) {
-    return;
-  }
-  const command = commands.get(args[0]);
-  if (command === undefined) {
-    return;
-  }
-  command(msg, args);
-});
 
 bot.on("ready", (): void => {
   console.log("logged in");
 });
+
+registerCommands(bot);
 
 await bot.login(process.env.DISCORD_BOT_TOKEN);
