884fc157fcbac53641e1350a0f0bb8339830ff5a
beepboop271 <53926222+beepboop271@users.noreply.github.com>
2021-04-12 04:37:28 -0400

    add db
    for future use to store ids

---
+++ src/db.ts
@@ -0,0 +1,25 @@
+import lowdb from "lowdb";
+import FileAsync from "lowdb/adapters/FileAsync";
+
+import { config } from "./config";
+
+// hard to type this better since the specific roles/channels
+// will somewhat depend on config, however the rest of the
+// bot will probably hardcode strings that are written in
+// config too (e.g. access role id of the "mentor" role)
+// so not sure what to do...
+interface GuildInfo {
+  roles: {
+    [key: string]: string;
+  };
+  channels: {
+    [key: string]: string;
+  };
+}
+
+interface Schema {
+  [key: string]: GuildInfo | undefined;
+}
+
+const adapter = new FileAsync<Schema>(config.dbFile);
+export const db = await lowdb(adapter);
