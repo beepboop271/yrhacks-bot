b09669870237433c7a714eef096a7c875c728fa9
beepboop271 <53926222+beepboop271@users.noreply.github.com>
2021-04-23 07:34:26 -0400

    add logging to censoring

--- src/censoring.ts
+++ src/censoring.ts
@@ -1,14 +1,19 @@
-import { Client, Permissions } from "discord.js";
+import { Client, MessageEmbed, Permissions } from "discord.js";
 
 import { config } from "./config";
+import { fetchChannel, fetchGuild } from "./db";
+import { isGuildMessage, makeUserString, mention } from "./utils";
 
 export const registerCensoring = (bot: Client): void => {
   bot.on("message", async (msg): Promise<void> => {
-    if (!msg.deletable) {
+    if (!msg.deletable || !isGuildMessage(msg)) {
       return;
     }
-    if (msg.member?.hasPermission(Permissions.FLAGS.MANAGE_MESSAGES) === true) {
-      // admins exempt (e.g. to do !wordunban <bannedword>)
+    if (
+      msg.member.hasPermission(Permissions.FLAGS.MANAGE_MESSAGES)
+      && msg.content.startsWith(`${config.prefix}word_unban`)
+    ) {
+      // kinda sketchy, admins exempt to do !wordunban <bannedword>
       return;
     }
 
@@ -17,10 +22,45 @@ export const registerCensoring = (bot: Client): void => {
     content = content.replace(/\s+/g, "");
     // TODO: common substitutions? eg 0 -> o
 
+    let matchedWord;
     for (const word of config.wordlist) {
       if (content.includes(word)) {
+        matchedWord = word;
         await msg.delete({ reason: `Found inappropriate word: ${word}` });
+        await msg.reply("watch your language.");
+        break;
       }
     }
+    if (matchedWord === undefined) {
+      return;
+    }
+
+    const db = fetchGuild(msg.guild);
+    if (db === undefined) {
+      return;
+    }
+    const channel = fetchChannel(msg.guild, db.channels.log);
+    if (channel === undefined || !channel.isText()) {
+      return;
+    }
+
+    await channel.send(new MessageEmbed({
+      title: "Wordlist Match",
+      author: {
+        name: makeUserString(msg.author),
+      },
+      description: `In ${mention(msg.channel)}:\n${msg.content}`,
+      fields: [
+        {
+          name: "Matched content:",
+          value: matchedWord,
+          inline: true,
+        },
+      ],
+      timestamp: Date.now(),
+      footer: {
+        text: msg.id,
+      },
+    }));
   });
 };
